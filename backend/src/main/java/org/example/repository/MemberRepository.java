8542c19883df5d854e6fe853f470be342a05e5e0
huwuxtins <nguyenhuutin124@gmail.com>
2023-10-13 19:56:56 +0700

    push backend

---
+++ backend/src/main/java/org/example/repository/MemberRepository.java
@@ -0,0 +1,16 @@
+package org.example.repository;
+
+import org.example.model.Member;
+import org.example.model.Project;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, UUID> {
+}
